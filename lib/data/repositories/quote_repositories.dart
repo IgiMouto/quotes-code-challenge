6ac26e43fb994d065dd3d2db78cfb24231919bf3
Luigi Angelo Siguenza <igi.siguenza@gmail.com>
2023-03-06 01:40:19 +0800

    :hammer: Change the return type when fetching data

--- lib/data/repositories/quote_repositories.dart
+++ lib/data/repositories/quote_repositories.dart
@@ -1,15 +1,18 @@
+import 'dart:convert';
+
 import 'package:quotes_code_challenge/data/models/quote_model.dart';
 import 'package:quotes_code_challenge/data/network/quote_api.dart';
 
 class QuoteRepository {
-  Future<Quote?> fetchQuotes() async {
+  Future<List<Quote>> fetchQuotes() async {
     final quoteApi = QuoteApi();
 
     try {
       final response = await quoteApi.fetchQuotes();
 
       if (response.statusCode == 200) {
-        return quoteFromJson(response.body);
+        List<dynamic> quotesJson = jsonDecode(response.body)['results'];
+        return quotesJson.map((element) => Quote.fromJson(element)).toList();
       } else {
         throw Exception(response.body);
       }
