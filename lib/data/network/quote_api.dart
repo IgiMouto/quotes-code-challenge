4dd6289b27fcc0d7082c961215ce1ced859a5712
Luigi Angelo Siguenza <igi.siguenza@gmail.com>
2023-03-06 01:39:43 +0800

    :hammer: Use correct endpoint

--- lib/data/network/quote_api.dart
+++ lib/data/network/quote_api.dart
@@ -1,8 +1,10 @@
 import 'package:http/http.dart' as http;
+import 'package:quotes_code_challenge/data/models/quote_model.dart';
 
 class QuoteApi {
   Future<http.Response> fetchQuotes() async {
-    final uriParsed = Uri.parse("https://quotable.io/quotes");
+    final uriParsed =
+        Uri.parse("https://api.quotable.io/quotes?page=1&limit=10");
 
     final response = await http.get(uriParsed);
 
