7dcd23960298fc923731d02291613661d388f5ad
Luigi Angelo Siguenza <igi.siguenza@gmail.com>
2023-03-06 18:40:31 +0800

    :hammer: Apply new changes in bloc to screens

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -22,54 +22,57 @@ class HomePage extends StatelessWidget {
         children: [
           BlocBuilder<QuoteBloc, QuoteState>(
             builder: (context, state) {
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: state.staticQuote.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => QuotePage(
-                              quote: state.staticQuote[index],
+              if (state.staticQuote != null) {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: state.staticQuote?.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => QuotePage(
+                                quote: state.staticQuote![index],
+                              ),
+                            ),
+                          );
+                        },
+                        child: Card(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Flexible(
+                                  child: Text(
+                                    state.staticQuote![index].author!,
+                                    style: const TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Flexible(
+                                  child: Text(
+                                    state.staticQuote![index].content!,
+                                    style: const TextStyle(
+                                        color: Colors.blue,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                        );
-                      },
-                      child: Card(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Flexible(
-                                child: Text(
-                                  state.staticQuote[index].author!,
-                                  style: const TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Flexible(
-                                child: Text(
-                                  state.staticQuote[index].content!,
-                                  style: const TextStyle(
-                                      color: Colors.blue,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w400),
-                                ),
-                              )
-                            ],
-                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              );
+                      );
+                    },
+                  ),
+                );
+              }
+              return Container();
             },
           )
         ],
--- lib/presentation/pages/quote_page.dart
+++ lib/presentation/pages/quote_page.dart
@@ -3,9 +3,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quotes_code_challenge/data/models/quote_model.dart';
 import 'package:quotes_code_challenge/logic/bloc/fetch_quote_bloc/quote_bloc.dart';
 
-class QuotePage extends StatelessWidget {
+class QuotePage extends StatefulWidget {
   const QuotePage({super.key, required this.quote});
   final Quote quote;
+
+  @override
+  State<QuotePage> createState() => _QuotePageState();
+}
+
+class _QuotePageState extends State<QuotePage> {
+  String? newContent;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,15 +32,20 @@ class QuotePage extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              quote.author!,
+              widget.quote.author!,
               style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             Flexible(
               child: TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    newContent = value;
+                  });
+                },
                 style:
                     const TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
                 maxLines: null,
-                initialValue: quote.content,
+                initialValue: widget.quote.content,
                 decoration: const InputDecoration(
                   contentPadding: EdgeInsets.all(5),
                   border: OutlineInputBorder(
@@ -42,15 +54,61 @@ class QuotePage extends StatelessWidget {
                 ),
               ),
             ),
-            ElevatedButton(
-                onPressed: () {
-                  context.read<QuoteBloc>().add(QuoteDeleting(quote: quote));
-                  Navigator.pop(context);
-                },
-                child: const Text("Delete"))
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      final result = await _buildDialog(context);
+                      if (result) {
+                        if (!mounted) return;
+                        context
+                            .read<QuoteBloc>()
+                            .add(QuoteDeleting(quote: widget.quote));
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Text("Delete")),
+                const SizedBox(width: 30),
+                ElevatedButton(
+                    onPressed: () {
+                      if (newContent != null) {
+                        context.read<QuoteBloc>().add(QuoteUpdating(
+                            quote: widget.quote, newContent: newContent!));
+                      }
+
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Update")),
+              ],
+            )
           ],
         ),
       ),
     );
+  }
+
+  Future<dynamic> _buildDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Are you sure you want to delete?"),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+                child: const Text("Yes"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+                child: const Text("No"),
+              ),
+            ],
+          );
+        });
   }
 }
