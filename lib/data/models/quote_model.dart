b77db930418ebd7964aa8e73cb2e6305d29d0638
Luigi Angelo Siguenza <igi.siguenza@gmail.com>
2023-03-06 18:39:21 +0800

    :hammer: Add id data

--- lib/data/models/quote_model.dart
+++ lib/data/models/quote_model.dart
@@ -1,16 +1,22 @@
 class Quote {
+  String? id;
   String? content;
   String? author;
-  Quote({this.content, this.author});
+  Quote({this.content, this.author, this.id});
 
   Quote copywith({
+    String? id,
     String? content,
     String? author,
   }) =>
-      Quote(content: content ?? this.content, author: author ?? this.author);
+      Quote(
+          id: id ?? this.id,
+          content: content ?? this.content,
+          author: author ?? this.author);
 
   factory Quote.fromJson(Map<String, dynamic> json) {
     return Quote(
+      id: json['_id'],
       content: json['content'],
       author: json['author'],
     );
